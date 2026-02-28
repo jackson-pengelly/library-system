d4f40c68bb1e373dd44729d3d0a7cec0a74c7c7b
Jackson <Jackson@DESKTOP-7PCOFV1>
2026-02-28 12:58:42 -0500

    Finished Member data model.

--- src/main/java/com/jacksonpengelly/models/Book.java
+++ src/main/java/com/jacksonpengelly/models/Book.java
@@ -18,7 +18,7 @@ public class Book {
 	Book() {
 
 	}
-	
+
 	// constructor for creating new book
 	Book(String isbn, String title, String author, String genre, int totalCopies) {
 		this.isbn = isbn;
@@ -27,9 +27,10 @@ public class Book {
 		this.genre = genre;
 		this.totalCopies = totalCopies;
 	}
-	
+
 	// constructor for loading from db
-	Book(int bookID, String isbn, String title, String author, String publisher, LocalDate publicationYear, String genre, int totalCopies, int availableCopies, LocalDate dateAdded) {
+	Book(int bookID, String isbn, String title, String author, String publisher, LocalDate publicationYear,
+			String genre, int totalCopies, int availableCopies, LocalDate dateAdded) {
 		this.bookID = bookID;
 		this.isbn = isbn;
 		this.title = title;
@@ -41,96 +42,90 @@ public class Book {
 		this.availableCopies = availableCopies;
 		this.dateAdded = dateAdded;
 	}
-	
+
 	// method to check if book is available
 	public boolean bookAvailable() {
 		return availableCopies > 0;
 	}
-	
+
 	// return formatted string with book info
+	@Override
 	public String toString() {
-		return String.format("ID: %d | ISBN: %s | Title: %s | Author: %s | Genre %s | Available: %d/%d", bookID, isbn, title, author, genre, availableCopies, totalCopies);
+		return String.format("ID: %d | ISBN: %s | Title: %s | Author: %s | Genre %s | Available: %d/%d", bookID, isbn,
+				title, author, genre, availableCopies, totalCopies);
 	}
-	
+
 	// getters
 	public int getBookID() {
 		return bookID;
 	}
-	
+
 	public String getISBN() {
 		return isbn;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public String getAuthor() {
 		return author;
 	}
-	
+
 	public String getPublisher() {
 		return publisher;
 	}
-	
+
 	public LocalDate getPublicationYear() {
 		return publicationYear;
 	}
-	
+
 	public String getGenre() {
 		return genre;
 	}
-	
+
 	public int getTotalCopies() {
 		return totalCopies;
 	}
-	
+
 	public int getAvailableCopies() {
 		return availableCopies;
 	}
-	
+
 	public LocalDate getDateAdded() {
 		return dateAdded;
 	}
-	
+
 	// setters
-	public void setBookID(int newBookID) {
-		bookID = newBookID;
+	public void setISBN(String isbn) {
+		this.isbn = isbn;
 	}
 	
-	public void setISBN(String newISBN) {
-		isbn = newISBN;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 	
-	public void setTitle(String newTitle) {
-		title = newTitle;
+	public void setAuthor(String author) {
+		this.author = author;
 	}
 	
-	public void setAuthor(String newAuthor) {
-		author = newAuthor;
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
 	}
 	
-	public void setPublisher(String newPublisher) {
-		publisher = newPublisher;
+	public void setPublicationYear(LocalDate publicationYear) {
+		this.publicationYear = publicationYear;
 	}
 	
-	public void setPublicationYear(LocalDate newPublicationYear) {
-		publicationYear = newPublicationYear;
+	public void setGenre(String genre) {
+		this.genre = genre;
 	}
 	
-	public void setGenre(String newGenre) {
-		genre = newGenre;
+	public void setTotalCopies(int totalCopies) {
+		this.totalCopies = totalCopies;
 	}
 	
-	public void setTotalCopies(int newTotalCopies) {
-		totalCopies = newTotalCopies;
-	}
-	
-	public void setAvailableCopies(int newAvailableCopies) {
-		availableCopies = newAvailableCopies;
-	}
-	
-	public void setDateAdded(LocalDate newDateAdded) {
-		dateAdded = newDateAdded;
+	public void setAvailableCopies(int availableCopies) {
+		this.availableCopies = availableCopies;
 	}
 }
--- src/main/java/com/jacksonpengelly/models/Member.java
+++ src/main/java/com/jacksonpengelly/models/Member.java
@@ -1,5 +1,132 @@
 package com.jacksonpengelly.models;
 
-public class Member {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+public class Member {
+	private int memberID;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private String address;
+	private LocalDate joinDate;
+	private boolean premium;
+	private boolean active;
+	private LocalDateTime creationDate;
+
+	// empty constructor
+	public Member() {
+
+	}
+
+	// constructor for adding new member
+	public Member(String firstName, String lastName, String email, String phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+
+	// constructor for loading from db
+	public Member(int memberID, String firstName, String lastName, String email, String phoneNumber, String address,
+			LocalDate joinDate, boolean premium, boolean active, LocalDateTime creationDate) {
+		this.memberID = memberID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		this.joinDate = joinDate;
+		this.premium = premium;
+		this.active = active;
+		this.creationDate = creationDate;
+	}
+
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
+
+	public String toString() {
+		return String.format("ID: %d | Name: %s %s | Email: %s | Phone: %s | Type: %s | Active: %s", memberID,
+				firstName, lastName, email, phoneNumber, premium ? "Premium" : "Standard", active ? "Yes" : "No");
+	}
+
+	// getters
+	public int getMemberID() {
+		return memberID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public LocalDate getJoinDate() {
+		return joinDate;
+	}
+
+	public boolean isPremium() {
+		return premium;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public LocalDateTime getCreationDate() {
+		return creationDate;
+	}
+
+	// setters
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setPremium(boolean premium) {
+		this.premium = premium;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public void setJoinDate(LocalDate joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public void setCreationDate(LocalDateTime creationDate) {
+		this.creationDate = creationDate;
+	}
 }
